446514df292f6ca9059157f1348992df4f8e1998
S-Masakatsu <s.masakatsu197@gmail.com>
2019-12-27 09:45:52 +0900

    [add]webSocket start

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import 'core-js'
 import 'regenerator-runtime/runtime'
 import _ from 'lodash'
 import 'style'
+import {startSocket} from 'app'
 
-(()=> {
-    
-})();
+startSocket('ws://localhost:8080');
