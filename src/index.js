2d4d463176244d30cf212a4628c66d581fba917b
S-Masakatsu <s.masakatsu197@gmail.com>
2020-01-02 17:38:02 +0900

    [updata]start top display

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import 'core-js'
 import 'regenerator-runtime/runtime'
 import _ from 'lodash'
 import 'style'
-import {startSocket} from 'app'
+import {startSocket, startTop} from 'app'
 
 startSocket('ws://localhost:8080');
+
+(() => {
+    startTop();
+})();
