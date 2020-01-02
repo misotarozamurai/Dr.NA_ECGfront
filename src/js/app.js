5ca0305e34faf5ee5a4fd6134a6b256cdf2ae481
S-Masakatsu <s.masakatsu197@gmail.com>
2020-01-02 17:39:18 +0900

    [add]control the entire

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,38 @@
 
 import WsSock from 'socket'
 import {startResultECG} from 'component'
+import {createTopDisplay, resetTopDisplay} from 'component/top'
+import {removeWrapperChild, removeSpecificChild} from 'element'
+
+//--------------------------------------------------------------------------
+// Control the entire application
+//--------------------------------------------------------------------------
 
 // ----- Start a webSocket server connection -----
 export const startSocket = url => new WsSock(url);
 
+// ----- Create the top screen -----
+export const startTop = () => createTopDisplay();
+
 // ----- Display the result using message data -----
 export const startResult = data => {
+    resetTopDisplay();
     const pulses = data.pulse;
     const height = data.height;
     startResultECG(pulses, height);
+}
+
+// ----- End the analysis and return to the top screen -----
+export const endResult = async() => {
+    await sleep(2000);
+    removeSpecificChild('main', 'result-video');
+    removeWrapperChild();
+    startTop();
+}
+
+// ----- Stop processing temporarily -----
+const sleep = msec => {
+    return new Promise( resolve => {
+       setTimeout( () => {resolve()}, msec);
+    });
 }
